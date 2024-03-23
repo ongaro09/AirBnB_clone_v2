4cc86c123626a0108b426f3d15770627da393727
nessy254 <mukaminessy9@gmail.com>
2024-03-23 22:17:39 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -2,6 +2,7 @@
 """ User Module for HBNB project """
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
@@ -11,4 +12,9 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
+
+    places = relationship(
+            'Place', backref='user', cascade='all, delete')
+    reviews = relationship(
+            'Review', backref='user', cascade='all, delete')
 
