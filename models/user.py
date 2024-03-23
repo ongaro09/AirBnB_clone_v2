4a6c9601d056ca5cfbfd00f817e1bc5d7c9a5947
nessy254 <mukaminessy9@gmail.com>
2024-03-23 22:19:36 +0300

    user.py

--- models/user.py
+++ models/user.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-""" User Module for HBNB project """
+"""This module defines the User class"""
 from sqlalchemy import Column, String
 from models.base_model import BaseModel, Base
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """ The user class, contains email, password, first and last name """
+    """ This class representing a user by various attributes"""
+
     __tablename__ = "users"
+
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=True)
     last_name = Column(String(128), nullable=True)
 
-    places = relationship(
-            'Place', backref='user', cascade='all, delete')
-    reviews = relationship(
-            'Review', backref='user', cascade='all, delete')
-
+    # Relationships
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
