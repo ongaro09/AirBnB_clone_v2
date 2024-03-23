d4e2bd2442ed1095bd6550e5f1cb5d89efd9d7f2
nessy254 <mukaminessy9@gmail.com>
2024-03-24 01:09:44 +0300

    amenities update

--- models/amenity.py
+++ models/amenity.py
@@ -14,7 +14,7 @@ class Amenity(BaseModel, Base):
                                    viewonly=False)
 
 
-place_amenity = Table('place_amenity', Base.metadata,
+    place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60), ForeignKey('places.id'),
                              primary_key=True, nullable=False),
                       Column('amenity_id', String(60),
