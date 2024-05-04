cbb220b3df44990fceda9b1dcd305fb7665e4bf0
ongaro09 <ongaromark7@gmail.com>
2024-05-04 08:33:12 +0300

    fixed PEP8 compliant issues

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -11,11 +11,10 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            return {key: value for key, value in FileStorage.__objects.items() if
-                    isinstance(value, cls)}
+            return {key: value for key, value in FileStorage.__objects.items()
+                    if isinstance(value, cls)}
         else:
             return FileStorage.__objects
-
 
     def new(self, obj):
         """Adds new object to storage dictionary"""
@@ -41,24 +40,25 @@ class FileStorage:
         from models.review import Review
 
         classes = {
-                    'BaseModel': BaseModel, 'User': User, 'Place': Place,
-                    'State': State, 'City': City, 'Amenity': Amenity,
-                    'Review': Review
-                  }
+            'BaseModel': BaseModel, 'User': User, 'Place': Place,
+            'State': State, 'City': City, 'Amenity': Amenity,
+            'Review': Review
+        }
         try:
             temp = {}
             with open(FileStorage.__file_path, 'r') as f:
                 temp = json.load(f)
                 for key, val in temp.items():
-                        self.all()[key] = classes[val['__class__']](**val)
+                    self.all()[key] = classes[val['__class__']](**val)
         except FileNotFoundError:
             pass
 
-     def delete(self, obj=None):
+    def delete(self, obj=None):
         """Deletes an object from __objects if it's inside"""
         if obj is not None:
             key = obj.__class__.__name__ + '.' + obj.id
             FileStorage.__objects.pop(key, None)
 
     def close(self):
+        """Reloads the storage"""
         self.reload()
