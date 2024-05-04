0fceef3cdca8338b69615061f1e9530fb666f34b
ongaro09 <ongaromark7@gmail.com>
2024-05-04 08:27:14 +0300

    improves engine

--- models/state.py
+++ models/state.py
@@ -10,20 +10,25 @@ from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
-    """
-    State class
-    """
-    __tablename__ = "states"
-    name = Column(String(128), nullable=False)
-
-    if getenv("HBNB_TYPE_STORAGE") == "db":
-        cities = relationship("City",  backref="state", cascade="delete")
+    """Representation of state """
+    if models.storage_t == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
     else:
+        name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes state"""
+        super().__init__(*args, **kwargs)
+
+    if models.storage_t != "db":
         @property
         def cities(self):
-            """Get a list of all related City objects."""
-            my_list = []
-            for item in list(models.storage.all(City).values()):
-                if item.state_id == self.id:
-                    my_list.append(item)
-            return my_list
+            """getter for list of city instances related to the state"""
+            city_list = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    city_list.append(city)
+            return city_list
