8a74ff3ff0db9c94cf0373d4923b932249ce3890
ongaro09 <ongaromark7@gmail.com>
2024-05-17 01:16:25 +0000

    modified

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""A script that starts a flask web application
+"""A script that starts a Flask web application
 Your web application must be listening on 0.0.0.0, port 5000
 """
 
@@ -7,12 +7,13 @@ from flask import Flask
 
 app = Flask("__name__")
 
+print(" * Serving Flask app '0-hello_route' (lazy loading)")
 
 @app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """Return a given string"""
-    return ("Hello HBNB!")
-
+    return "Hello HBNB!"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5000, debug=False)
+
